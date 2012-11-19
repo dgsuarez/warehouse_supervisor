5618a8a71df7e89a949c64a58cbbaa240ba45b27
Diego Guerra <diego.guerra.suarez@gmail.com>
2012-11-19 20:52:48 +0100

    sample config file

---
+++ spec/example.rb
@@ -0,0 +1,24 @@
+define_options :production do |opts|
+  opts.user = ENV["USER"]
+  opts.process_name = "%(program_name)_%(process_number)" 
+  opts.environment = {
+    :HOME => ENV["HOME"],
+    :RAILS_ENV => ENV["RAILS_ENV"]
+  }
+end
+
+define_program :resque_web, :command => "bundle exec resque-web"
+
+define_program :resque_scheduler do |program|
+  program[:command] = "bundle exec rake resque:scheduler"
+end
+
+define_program :resque_worker do |program|
+  program.command = "bundle exec rake resque:work"
+end
+
+with_options :production do |out| 
+  out.program :resque_worker, :num_procs => 3
+  out.program :resque_web
+  out.program :resque_scheduler
+end
