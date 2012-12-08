8b63b00a64e682ea8caf72474b7b35fe0bf0e9a5
Diego Guerra <diego.guerra.suarez@gmail.com>
2012-12-08 18:02:26 +0100

    refactor

--- lib/warehouse_supervisor/outputter.rb
+++ lib/warehouse_supervisor/outputter.rb
@@ -5,24 +5,20 @@ module WarehouseSupervisor
       @model = model
     end
 
-    def output_program(name, prog)
-      out = []
-      out << "[program:#{name}]"
-      prog.each do |name, val|
+    def output_program(prog_name, opts)
+      opt_list = opts.map do |name, val|
         res = "#{name} = "
         if val.kind_of? Hash
-          r = []
-          val.each do |opt_name, opt_value|
-            r << "#{opt_name}=\"#{opt_value}\""
-          end
-          res << r.join(",")
+          res + hash_to_csv(val)
         else
-          res << val
+          res + val
         end
-        out << res
       end
-      out.join("\n") + "\n"
+      ["[program:#{prog_name}]"].concat(opt_list).join("\n") + "\n"
     end
-    
+
+    def hash_to_csv(hash)
+      hash.map{|k, v| "#{k}=\"#{v}\""}.join(",")
+    end
   end
 end
