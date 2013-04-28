8942048a399c921044a3e13a8cbba545a1027d6a
Diego Guerra <diego.guerra.suarez@gmail.com>
2013-04-28 19:59:21 +0200

    add start command to generate file and run supervisor in fg

--- lib/warehouse_supervisor_cli.rb
+++ lib/warehouse_supervisor_cli.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'yaml'
+require 'tempfile'
+require 'fileutils'
 require 'thor'
 require 'warehouse_supervisor/renderer'
 
@@ -9,9 +11,28 @@ module WarehouseSupervisor
     class_option :group, :aliases => :g, :default => "development"
     class_option :config, :aliases => :c, :required => true
 
-    desc "print erb_file", "output the file"
+    desc "print file", "output the file acording to the config"
     def print(erb_file)
-      puts generate_contents(options[:group] || "development", options[:config], erb_file)
+      puts generate_contents(options[:group], options[:config], erb_file)
+    end
+
+    desc "start file", "start an undemonized supervisor with file"
+    option :log_dir, :aliases => :q, :default => "log"
+    def start(erb_file)
+      Tempfile.open("supervisord.conf") do |f|
+        log_dir = options[:log_dir]
+        f.puts "[supervisord]"
+        f.puts "logfile = #{File.join(log_dir, "supervisord.log")}"
+        f.puts "childlogdir = #{log_dir}"
+        f.puts "nodaemon = true"
+        f.puts
+        f.write generate_contents(options[:group], options[:config], erb_file)
+        f.flush
+        FileUtils.mkdir_p options[:log_dir]
+        command = "supervisord -c '#{f.path}' -n"
+        puts command
+        exec command
+      end
     end
 
     private 
